e8c946233a36dd06de584ceebada35ac691fa78b
LilianDamico <lfonseca_damico@hotmail.com>
2025-03-23 10:44:51 -0300

    ...

--- src/components/chatbot/Chatbot.js
+++ src/components/chatbot/Chatbot.js
@@ -13,7 +13,7 @@ const Chatbot = () => {
     setMessages([...messages, userMessage]);
 
     try {
-      const response = await fetch("https://chatbot-back-p11b.onrender.com/", {
+      const response = await fetch("https://chatbot-back-p11b.onrender.com/perguntar", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: "Sua pergunta aqui" }),
